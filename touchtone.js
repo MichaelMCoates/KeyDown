57ef4adde30cf1dd3234e1ddc044177482b0415f
Michael Coates <MichaelMCoates@gmail.com>
2017-07-18 18:06:42 -0400

    Change positioning with keys

--- touchtone.js
+++ touchtone.js
@@ -1,5 +1,58 @@
 import anime from 'animejs';
 
+let topLeftKeys = {
+  "1": true,
+  "2": true,
+  "3": true,
+  "4": true,
+  "5": true,
+  "q": true,
+  "w": true,
+  "e": true,
+  "r": true,
+  "t": true,
+};
+
+let topRightKeys = {
+  "6": true,
+  "7": true,
+  "8": true,
+  "9": true,
+  "0": true,
+  "y": true,
+  "u": true,
+  "i": true,
+  "o": true,
+  "p": true,
+};
+
+let bottomLeftKeys = {
+  "a": true,
+  "s": true,
+  "d": true,
+  "f": true,
+  "g": true,
+  "z": true,
+  "x": true,
+  "c": true,
+  "v": true,
+  "b": true,
+};
+
+let bottomRightKeys = {
+  "h": true,
+  "j": true,
+  "k": true,
+  "l": true,
+  ";": true,
+  "n": true,
+  "m": true,
+  ",": true,
+  ".": true,
+  "/": true,
+};
+
+
 window.human = false;
 
 var canvasEl = document.querySelector('.fireworks');
@@ -7,6 +60,7 @@ var ctx = canvasEl.getContext('2d');
 var numberOfParticules = 30;
 var pointerX = 0;
 var pointerY = 0;
+var section = "";
 var tap = ('ontouchstart' in window || navigator.msMaxTouchPoints) ? 'touchstart' : 'mousedown';
 var colors = ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C'];
 
@@ -19,19 +73,7 @@ function setCanvasSize() {
 }
 
 function topLeft(key) {
-  let topLeftKeys = {
-    "1": true,
-    "2": true,
-    "3": true,
-    "4": true,
-    "5": true,
-    "q": true,
-    "w": true,
-    "e": true,
-    "r": true,
-    "t": true,
-    "y": true,
-  };
+
   if (topLeftKeys[key]) {
     return true;
   } else {
@@ -39,14 +81,49 @@ function topLeft(key) {
   }
 }
 
-function setCoords(key) {
-  if (topLeft(key)) {
-    return [Math.random() * window.innerWidth / 2, window.innerHeight / 2];
+function setSection(key) {
+  if (topLeftKeys[key]) {
+    section = "Top Left";
+  } else if (topRightKeys[key]) {
+    section = "Top Right";
+  } else if (bottomLeftKeys[key]) {
+    section = "Bottom Left";
+  } else if (bottomRightKeys[key]) {
+    section = "Bottom Right";
+  }
+}
+
+function setCoords() {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  switch (section) {
+    case "Top Left":
+      return [
+        Math.random() * width / 2,
+        Math.random() * height / 2
+      ];
+    case "Top Right":
+      return [
+        Math.random() * (width - width / 2) + width / 2,
+        Math.random() * height / 2
+      ];
+    case "Bottom Left":
+      return [
+        Math.random() * width / 2,
+        Math.random() * (height - height / 2) + height / 2
+      ];
+    case "Bottom Right":
+      return [
+        Math.random() * (width - width / 2) + width / 2,
+        Math.random() * (height - height / 2) + height / 2
+      ];
+    default:
+
   }
 }
 
 function updateCoords(e) {
-  let newCoords = setCoords(e.key);
+  let newCoords = setCoords();
   pointerX = newCoords[0];
   pointerY = newCoords[1];
   // pointerX = e.clientX || e.touches[0].clientX;
@@ -148,6 +225,7 @@ var render = anime({
 document.addEventListener('keydown', function(e) {
   window.human = true;
   render.play();
+  setSection(e.key);
   updateCoords(e);
   animateParticules(pointerX, pointerY);
 }, false);
