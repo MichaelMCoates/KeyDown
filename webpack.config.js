e707129e8b8bb6d3e1c951232be968e20ff6de28
Michael Coates <MichaelMCoates@gmail.com>
2017-07-18 17:18:29 -0400

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+module.exports = {
+  context: __dirname,
+  entry: './touchtone.js',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+};
